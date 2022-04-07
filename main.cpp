70ad691ea41ba5529906e00f69d232cf7ef90e48
deekep <MVSein@stud.kantiana.ru>
2022-04-07 09:16:54 +0200

    edit

--- main.cpp
+++ main.cpp
@@ -2,11 +2,11 @@
 
 int main() {
     BMP image;
-    image.readBMP("/Users/maksimsein/Documents/gitKraken/lab-2-9/in");
+    image.readBMP("../in");
 
     image.inverseFilter();
 
-    image.writeBMP("/Users/maksimsein/Documents/gitKraken/lab-2-9/out");
+    image.writeBMP("../out");
 
     return 0;
 }
